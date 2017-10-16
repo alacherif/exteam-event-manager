aa4db3ee968d1bdc841a5d81923c5671c269597f
OUARRICH <warichsaid@gmail.com>
2017-10-16 11:15:01 +0100

    init Http Request

--- eventmanager-front/src/app/services/event.service.ts
+++ eventmanager-front/src/app/services/event.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise'; 
 
 import { EVENTS } from '../api/mock-events';
 import { EventObj } from '../shared/event/EventObj';
 
 @Injectable()
 export class EventService{
+    private eventsUrl = 'http://localhost:3000/events';
+
+    constructor(private http: Http){}
+
     getEvents(): Promise<EventObj[]>{
         return Promise.resolve(EVENTS);
     }
@@ -14,5 +21,17 @@ export class EventService{
           // Simulate server latency with 2 second delay
           setTimeout(() => resolve(this.getEvents()), 2000);
         });
+    }
+
+    getHeroes(): Promise<EventObj[]> {
+        return this.http.get(this.eventsUrl)
+                    .toPromise()
+                    .then(response => {response.json().data as EventObj[];})
+                    .catch(this.handleError);
+    }
+       
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error);
+        return Promise.reject(error.message || error);
     }
 }
