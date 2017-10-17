7377a2fa1cf729fdf5b4e027cf019533e347ea13
OUARRICH <warichsaid@gmail.com>
2017-10-17 13:13:33 +0100

    Update eventManager.component.ts

--- eventmanager-front/src/app/eventManager/eventManager.component.ts
+++ eventmanager-front/src/app/eventManager/eventManager.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+
 import { EventObj } from '../shared/event/EventObj';
 import { EventService } from '../services/event.service';
 
@@ -7,14 +8,13 @@ import { EventService } from '../services/event.service';
     templateUrl: 'event-manager.component.html'
 })
 export class EventManagerComponent{
-    title : string = "on est dans le backoffice"; 
+    title : string = 'Gestions des événements'; 
     eventsList: EventObj[];
 
     constructor(private eventService: EventService){}
     
     getEvents(): void{
         this.eventService.getEventsSlowly().then(events => this.eventsList = events);
-        //this.eventService.getEvents().then(events => this.eventsList = events);
     }
     
     ngOnInit(): void{
