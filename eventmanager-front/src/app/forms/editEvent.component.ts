0325c99b8173923417ff30071d342b1d6692cf98
ClubberBen <otmane.benammar@gmail.com>
2017-10-19 17:28:26 +0200

    Edit Resa (#11)
    * build forms with material components

--- eventmanager-front/src/app/forms/editEvent.component.ts
+++ eventmanager-front/src/app/forms/editEvent.component.ts
@@ -6,8 +6,7 @@ import { EventObj } from '../shared/event/EventObj';
 
 @Component({
     selector: 'edit-event',
-    templateUrl: 'editEvent.component.html',
-    providers : [EventService]
+    templateUrl: 'editEvent.component.html'
 })
 export class EditEventComponent{
     formTitle: string = 'Modification de l\'événement';
