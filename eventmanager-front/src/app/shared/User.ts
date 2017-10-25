57a26c30af7bc11bde4972f42469d3e8006480ad
ClubberBen <otmane.benammar@gmail.com>
2017-10-25 15:03:57 +0200

    FormValidation
    Form Validation & Directives (phonenumber, alphabetics inputs)

---
+++ eventmanager-front/src/app/shared/User.ts
@@ -0,0 +1,11 @@
+export class User{
+    constructor(
+        public entreprise: string,
+        public fullname: string,
+        public email: string,
+        public phone: number,
+        public commercial : string,
+        public commentaire : string
+
+    ){}
+} 
